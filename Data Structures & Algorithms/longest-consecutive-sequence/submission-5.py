cd18e7eb7864c0b10f50fd9be11d9d136a28ff6a
amir93 <amir.salah9300@gmail.com>
2026-04-18 04:06:23 +0200

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.py
@@ -0,0 +1,40 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        
+        
+        if nums == []:
+            return 0
+        
+        length = 0
+        index = []
+        # compute the index array size
+        max_nums = float("-inf")
+        min_nums = float("inf")
+        for e in nums:
+            if e > max_nums:
+                max_nums = e
+            if e < min_nums:
+                min_nums=e
+        
+        if min_nums > 0:
+            min_nums = 0
+        
+        index_size = max_nums + 1 - min_nums
+
+        index = [0 for _ in range(index_size)]
+        
+        for e in nums:
+            index[e-min_nums] = 1
+
+        max_length = 0
+        for e in index:
+            if e == 0:
+                if length > max_length:
+                    max_length = length
+                length = 0
+            else:
+                length +=1
+
+        if length > max_length:
+            max_length = length
+        return max_length
